d84066bfcc5c40443c2a437dabcf4261fb9c2425
Jonathan <jonathan.leal1231@gmail.com>
2024-03-13 17:54:40 -0400

    troubleshooting for previously skipped tests

--- tests/fractal_database_controller/test_sync_data.py
+++ tests/fractal_database_controller/test_sync_data.py
@@ -4,8 +4,10 @@ import pytest
 from fractal_database.controllers.fractal_database_controller import (
     FractalDatabaseController,
     RoomGetStateEventError,
+    Receiver,
 )
 from taskiq_matrix.filters import create_room_message_filter
+from taskiq_matrix.matrix_broker import MatrixBroker, AckableMessage
 
 FILE_PATH = "fractal_database.controllers.fractal_database_controller"
 FRACTAL_PATH = "fractal.matrix.FractalAsyncClient"
@@ -37,14 +39,18 @@ async def test_sync_data_no_tasks(test_room_id):
     mock_json_loads.assert_not_called()
 
 
-# @pytest.mark.skip(reason="figure out tasks, queues, and brokers. go back to taqskiq and test")
-async def test_sync_data_with_tasks(test_matrix_broker, test_multiple_broker_message):
+async def test_sync_data_with_tasks(test_matrix_broker, test_multiple_broker_message, test_room_id):
     """ """
 
-    test_broker = await test_matrix_broker()
-    test_broker.init_queues = MagicMock()
+    # create a FractalDatabaseController object
+    controller = FractalDatabaseController()
 
-    tasks = await test_multiple_broker_message(5)
+    test_broker: MatrixBroker = await test_matrix_broker()
+    test_broker._init_queues = MagicMock()
+
+    num_tasks = 5
+
+    tasks = await test_multiple_broker_message(num_tasks)
 
     for task in tasks:
         await test_broker.kick(task)
@@ -53,8 +59,26 @@ async def test_sync_data_with_tasks(test_matrix_broker, test_multiple_broker_mes
         test_broker.room_id, types=[test_broker.replication_queue.task_types.task]
     )
 
+
     returned_tasks = await test_broker.replication_queue.get_tasks(timeout=0, task_filter=task_filter)
 
-    print("right here============", returned_tasks[0])
+    for task in returned_tasks:
+        task.data['args'] = ['test_args']
 
-    # with patch("fractal_database_matrix.broker.broker", test_broker):
+    test_broker.replication_queue.get_tasks = AsyncMock(side_effect=[returned_tasks, []])
+
+    test_broker.replication_queue.yield_task = AsyncMock(spec=AckableMessage)
+
+    with patch("fractal_database_matrix.broker.broker", test_broker):
+
+        with patch(f"{FILE_PATH}.json.loads", return_value="test_args") as mock_loads:
+
+            with patch(f"{FILE_PATH}.Receiver.callback") as mock_callback:
+
+                await controller._sync_data(test_room_id)
+
+    assert test_broker.replication_queue.get_tasks.call_count == 2
+
+    assert mock_loads.call_count == num_tasks
+
+    test_broker.replication_queue.yield_task.assert_called_once()
--- tests/fractal_database_controller/test_sync_database_metadata.py
+++ tests/fractal_database_controller/test_sync_database_metadata.py
@@ -6,6 +6,7 @@ from fractal_database.controllers.fractal_database_controller import (
     FractalDatabaseController,
     RoomGetStateEventError,
 )
+from nio import RoomGetStateEventResponse
 
 FILE_PATH = "fractal_database.controllers.fractal_database_controller"
 FRACTAL_PATH = "fractal.matrix.FractalAsyncClient"
@@ -30,10 +31,33 @@ async def test_sync_database_RoomGetStateEventError(test_room_id, _use_django):
 
     assert str(e.value) == expected_message
 
-async def test_sync_error_parsing_database(test_room_id, _use_django):
+async def test_sync_database_error_parsing_database(test_room_id, _use_django):
     """
+    Tests that an exception is raised there is an error when trying to parse the database
     """
 
     controller = FractalDatabaseController()
 
-    with patch(f"{FILE_PATH}.")
+    with patch(f"{FILE_PATH}.json.loads", side_effect=Exception) as mock_json_loads:
+        with patch(f"{FRACTAL_PATH}.room_get_state_event") as mock_room_get_state:
+            response = MagicMock(spec=RoomGetStateEventResponse)
+            mock_room_get_state.return_value = response
+            with pytest.raises(Exception) as e:
+                await controller._sync_database_metadata(room_id=test_room_id)
+
+    assert 'Failed to parse database' in str(e.value)
+
+
+@pytest.mark.skip(reason='json.loads return type error')
+async def test_sync_database_RoomGetStateEventError_after_appending_fixture(new_matrix_room, _use_django, test_yaml_dict):
+    """
+    """
+
+    mock_json_data = {"fixture": [{"pk": 123}]}
+    controller = FractalDatabaseController()
+    with patch(f"{FILE_PATH}.json.loads", return_value=mock_json_data) as mock_loads:
+        with patch(f"{FRACTAL_PATH}.room_get_state_event") as mock_room_get_state:
+            response = MagicMock(spec=RoomGetStateEventResponse)
+            response.content = 'test_content'
+            mock_room_get_state.return_value = response
+            await controller._sync_database_metadata(room_id=new_matrix_room)
